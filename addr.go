88001c4f1b2f4f2ec86cc3cefbc7ae37e96be01d
Roger Helkey <roger@helkey.org>
2019-05-07 18:30:25 -0700

    Addr testing

--- addr.go
+++ addr.go
@@ -1,55 +1,77 @@
-// Encode
+// Addr: Get encoding addresses (incl base address from server)
+// $ go run addr.go encode.go
 
 package main
 
 import (
-	// "errors"
-	// "fmt"
+	// "encoding/binary"
+	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"time"
 )
 
-
 func main() {
-	// TestAddr()
+	// testAddr()
+	addrShard, err := getBaseAddrServer(UrlAddr)
+	if err == nil {
+		fmt.Println(addrShard)
+	} else {
+		fmt.Println("err:", err)
+	}
 }
 
-var nAddrs int = 1e6 // number of addresses offset from each base address
-var retryInterval time.Duration = 10 // interval to re-try address server (sec)
-var requestTimeout = retryInterval/2 // set timeouts to <retryInterval
-const urlAddr = "localhost:8080"
+func init() {
+	rand.Seed(time.Now().UnixNano()) // initialize random seed
+}
 
-func getAddr(urlAddr string, chAddr chan uint64) {
+type BaseShard struct {
+	addr  uint64
+	shard uint32
+}
+
+var nAddrs int = 1e6                   // number of addresses offset from each base address
+var retryInterval time.Duration = 10   // interval to re-try address server (sec)
+var requestTimeout = retryInterval / 2 // set timeouts < retryInterval
+const UrlAddr = "http://127.0.0.1:8088/addr"
+
+func getAddr(urlAddr string, chAddr chan BaseShard) {
 	// const nAddrs = 1e6
 	const chDepth = 1 // channel queue depth to store lookahead base addresses
 	chBase := make(chan uint64, chDepth)
 	go getBaseAddr(urlAddr, chBase)
+	shardMask := uint64(Nshard) - 1
+	baseMask := ^shardMask
+	// fmt.Printf("baseMask:%b  shardMask:%b", baseMask, shardMask)
 	for {
 		// Random array of 'nAddrs' addresses offset from
 		//    each base address from server
-		baseAddr := <- chBase // get base address from server
+		baseAddrShard := <-chBase // get base address from server
+		baseAddr := baseAddrShard & baseMask
+		baseShard := new(BaseShard)
+		baseShard.shard = uint32(baseAddrShard & shardMask)
 		addrs := rand.Perm(nAddrs) // select addr in random order
-		for addr := range addrs {
-			chAddr <- baseAddr + uint64(addr)
+		for _, addr := range addrs {
+			baseShard.addr = baseAddr + uint64(addr)
+			chAddr <- *baseShard
 		}
 	}
-}	
-
+}
 
 // Request/retry base address, database shard.
 //   Store using buffered go channel as queue
-func getBaseAddr(urlAddr string, chBase chan uint64)  {
-	var baseAddr uint64
+func getBaseAddr(urlAddr string, chBase chan uint64) {
+	var baseAddrShard uint64
 	var err error
 	for {
 		// Request base address range from server
-		baseAddr, err = requestAddr(urlAddr)
+		baseAddrShard, err = requestAddr(urlAddr)
 		if err != nil {
 			// Retry address server until responds
 			ticker := time.NewTicker(retryInterval * time.Second)
 			for _ = range ticker.C {
-				baseAddr, err = requestAddr(urlAddr)
+				baseAddrShard, err = requestAddr(urlAddr)
 				if err == nil {
 					break
 				}
@@ -58,28 +80,75 @@ func getBaseAddr(urlAddr string, chBase chan uint64)  {
 		}
 		// Send base address; channel blocks when full
 		// fmt.Print("\nNew base addr:", baseAddr, ": ")
-		chBase <- baseAddr
+		chBase <- baseAddrShard
 		// time.Sleep(1 * time.Second)
 	}
 }
 
-
 // Single request for base address, database shard from remote address server
-//   Note1: specify timeout (don't use default http request client)
+//   Note: specify timeout (don't use default http request client)
 //   TODO: re-write this with gRPC
-func getBaseAddrServer(url string) (uint64, error) {
+func getBaseAddrServer(urlAddr string) (uint64, error) {
+	var netClient = &http.Client{
+		Timeout: time.Second * requestTimeout,
+	}
+	resp, err := netClient.Get(urlAddr)
+	if err != nil {
+		fmt.Println("Failed netClient.Get")
+		return 0, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Failed ioutil.ReadAll")
+		return 0, err
+	}
+	fmt.Println("body:", body, string(body[0]))
+	// addrShard := binary.BigEndian.Uint64(body)
+	addrShard := uint64(1024)
+	return addrShard, nil
 }
+
 var requestAddr = getBaseAddrServer
 
-func Test() (chan uint64, uint64, int)  {
+// For automated testing. Overrides defaults, mocks address server
+func Test() chan BaseShard {
+	// Override operational defaults
 	nAddrs = 10 // number of address per base address
-	chAddr := make(chan uint64)
-	go getAddr(urlAddr, chAddr)
 	var base uint64 = 0
-	var baseIncr uint64 = 1000
+	var baseIncr uint64 = 1024
+	rand.Seed(0) // const seed for repeatible test results
 	requestAddr = func(url string) (uint64, error) {
 		base += baseIncr
 		return base, nil
 	}
-	return chAddr, baseIncr, nAddrs
+
+	chAddr := make(chan BaseShard)
+	go getAddr(UrlAddr, chAddr)
+	return chAddr
+}
+
+// For automated testing. Overrides defaults, mocks address server
+func Testserver() chan BaseShard {
+	// Override operational defaults
+	nAddrs = 10 // number of address per base address
+	rand.Seed(0) // const seed for repeatible test results
+
+	chAddr := make(chan BaseShard)
+	go getAddr(UrlAddr, chAddr) // UrlAddr defined in RequestAddr
+	return chAddr
+}
+
+// Used for diagnostics when getting failures using
+//  $ go test addr_test.go
+func testAddr() {
+	// chAddr := Test()
+	chAddr := Testserver()
+	const sleepMs = 10 // avoid race condition between channels
+	const nIter = 30
+	for i := 0; i < nIter; i++ {
+		addrShard := <-chAddr
+		fmt.Print(addrShard.addr, ", ")
+		time.Sleep(sleepMs * time.Millisecond)
+	}
 }
--- addr_test.go
+++ addr_test.go
@@ -1,27 +1,25 @@
-// Encode
+// Addr_test
+// $ go test addr_test.go addr.go encode.go
 
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
-	"github.com/stretchr/testify/assert"
-	// "github.com/gotestyourself/gotest.tools/assert"
 )
 
+var result = []uint64{1032, 1026, 1027, 1024, 1029, 1031, 1025, 1030, 1033, 1028,
+	2053, 2056, 2055, 2054, 2057, 2051, 2048, 2052, 2049, 2050,
+	3079, 3078, 3081, 3073, 3072, 3075, 3076, 3080, 3077, 3074}
 
 func TestAddr(t *testing.T) {
-	chAddr, baseIncr, nAddrs := Test()
+	chAddr := Test()
 	const sleepMs = 10 // avoid race condition between channels
 	const nIter = 30
-	for i:=0; i<nIter; i++ {
-		addr := <-chAddr
-		addrExpect := baseIncr * (uint64(i / nAddrs) + 1) + uint64(i % nAddrs)
-		assert.Equal(t, addr, addrExpect)
+	for i := 0; i < nIter; i++ {
+		addrShard := <-chAddr
+		assert.Equal(t, addrShard.addr, result[i])
 		time.Sleep(sleepMs * time.Millisecond)
 	}
 }
-
-
-
-
