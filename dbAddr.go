9114b3cffd946cdf15eab31b74178d7b4bf7a45e
Roger Helkey <roger@helkey.org>
2019-05-22 21:36:11 -0700

    dbAddr error handling

--- dbAddr.go
+++ dbAddr.go
@@ -1,10 +1,15 @@
+// dbAddr.go
+// pgstart (WSL)  # Starting PostgreSQL 10 database server
+// runpg (WSL)    # log into the psql prompt
+
 package main
 
 import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"log"
+	// "log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -12,23 +17,28 @@ import (
 const (
 	dbType = "postgres"
 	host   = "localhost"
-	port   = 5430 // plus shard #
-	user   = "postgres"
-	dbName = "addrUrlRand"
+	port   = 5432
+	user   = "helkey"
+	dbName = "addrurlrand"
 )
 
 // Database object pointer, shard
 type DBS struct {
-	db    *sql.DB
-	shard uint32
+	db     *sql.DB
+	shard  uint32
+	passwd string
 }
 
 func main() {
+	passwd := os.Args[1]
+	dbS := DBS{nil, 9999, passwd}
+	fmt.Println(dbS.OpenDB(0, passwd))
+	// CreateTables("")
 }
 
 //
-func (dbS DBS) OpenDB(passwd string, shard uint32) error {
-	if shard == dbS.shard {
+func (dbS DBS) OpenDB(shard uint32, passwd string) error {
+	if (passwd == dbS.passwd) && (shard == dbS.shard) {
 		return nil
 	}
 	dbInfo := fmt.Sprintf("host=%s port=%d user=%s "+
@@ -36,46 +46,74 @@ func (dbS DBS) OpenDB(passwd string, shard uint32) error {
 		host, port, user, passwd, dbName)
 	db, err := sql.Open(dbType, dbInfo)
 	if err != nil {
-		log.Fatal("CreateTable: not able to connect to database")
-		return err
+		e := "CreateTable: not able to connect to database"
+		fmt.Println(e)
+		// log.Fatal(e)
+		return errors.New(e)
 	}
-	dbS.db = db
-	dbS.shard = shard
+
+	fmt.Println("password:", passwd)
+	// sqlTbl := `CREATE DATABASE db0;`
+	// _, err = db.Exec(sqlTbl)
+	// fmt.Println("0:", err)
+
+	sqlTbl := `CREATE TABLE randext (addr INT, fullurl TEXT);`
+	_, err = dbS.db.Exec(sqlTbl)
+	fmt.Println("1:", err)
+	
+	dbS = DBS{db, shard, passwd}
 	return nil
+}
+
+func CreateTables(passwd string) {
+	if passwd == "" {
+		if len(os.Args) <= 1 {
+			fmt.Println("CreateTables: need Database password")
+			return
+		}
+		passwd = os.Args[1]
+	}
+	dbS := DBS{}
+	dbS.OpenDB(uint32(0), passwd)
+	fmt.Println(dbS.CreateTable(passwd))
+	// if err != nil {
+	// 	fmt.Println("CreateTables failed")
+	//}
 }
 
 // defer db.Close()
 // Create new DB shard / table
-func (dbS DBS) CreateTable(name string) error {
-	db := dbS.db
-	sqlTbl := `
-CREATE TABLE addr (
-  addr INT,
-  fullUrl TEXT,
-);
-CREATE TABLE rand (
-  addr INT,
-  randExt TEXT,
-);`
-	_, err := db.Exec(sqlTbl)
-	if err != nil {
-		return errors.New("")
-	}
-	return nil
+func (dbS DBS) CreateTable(passwd string) (err error) {
+	// Recover from db.Exec() panic
+	defer func() {
+		if r := recover(); r != nil {
+			e := "CreateTable: can't create database table"
+			err = errors.New(e)
+
+		}
+	}()
+
+	sqlTbl := `CREATE TABLE randext (addr INT, fullurl TEXT);`
+	_, err = dbS.db.Exec(sqlTbl)
+	// sqlTbl := `CREATE TABLE randext (addr INT, rand INT);`
+	// _, err = db.Exec(sqlTbl) 
+	return
 }
 
+	
 // Save shortened URL to DB
-func (dbS DBS) SaveAddr(fullUrl string, addr uint64, randExt uint32, passwd string, shard uint32) error {
-	err := dbS.OpenDB(passwd, shard)
+func (dbS DBS) SaveAddr(fullUrl string, addr uint64, randExt uint32, shard uint32, passwd string) error {
+	err := dbS.OpenDB(shard, passwd)
 	if err != nil {
 		return errors.New("SaveAddr: not able to connect to database")
 	}
-	sqlIns := `
-INSERT INTO fullUrl (addr, fullUrl)
-VALUES ($1, $2)
-INSERT INTO randExt (addr, randExt)
-VALUES ($3, $4)`
 	db := dbS.db
+
+	sqlIns := `
+INSERT INTO fullUrl` + string(shard) + ` (addr, fullUrl)
+VALUES ($1, $2);
+INSERT INTO randExt` + string(shard) + ` (addr, randExt)
+VALUES ($3, $4);`
 	_, err = db.Exec(sqlIns, addr, fullUrl)
 	if err != nil {
 		return errors.New("")
