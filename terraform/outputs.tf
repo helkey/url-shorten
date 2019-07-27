20f62f06a2c4bfadde0ce9ff35e250fc26a7ce72
helkey <roger@helkey.org>
2019-07-26 21:41:40 -0700

    Packer/Terraform integration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,12 +4,22 @@
 // value = "${aws_elb.web.dns_name}"
 //}
 
+output "aws_key_name" {
+  value="${aws_key_pair.auth.key_name}"
+}
+
+output "aws_public_key" {
+  value="${aws_key_pair.auth.public_key}"
+}
+  
+output "addr_ip" {
+  value="${aws_instance.addr_server.public_ip}"
+}
+
 // output "addr_shard0" {
 output "addr_dbAddr" {
   // value = "${aws_db_instance.db_shard0.address}"
-  value = "${aws_db_instance.db_addr.address}"
   // also: id of subnet group: aws_db_subnet_group.db_subnet_group.id
+  value = "${aws_db_instance.db_addr.address}"
 }
 
-
-  
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "public_key_path" {
 
 variable "key_name" {
   description = "key name"
-  default = "~/.ssh/authorized_keys/url-key-uswest.pem"
+  default = "~/.ssh/authorized_keys/url-key-uswest" // DONT add .pem!!!
 }
 
 variable "db_password" {
@@ -16,10 +16,11 @@ variable "aws_region" {
   default     = "us-west-1"
 }
 
-variable "aws_amis" {
+variable "addr_amis" {
   description = "Amazon instance machine images"
   default = {
-    us-west-1 = "ami-087fb2fd7e9c79e6e"
+    us-west-1 = "ami-056ee704806822732" // Unmodifed Amazon AMI
+    // not responding: ami-0e29c20cd0c57c5ed, ami-087fb2fd7e9c79e6e
     us-west-2 = "ami-052d9eee6d5a9bf35"
   }
 }
