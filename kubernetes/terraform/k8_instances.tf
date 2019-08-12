f9a40241c75d21ea059463203c5c5806c41ab022
helkey <roger@helkey.org>
2019-08-11 22:51:52 -0700

    IAM policies

--- kubernetes/terraform/k8_instances.tf
+++ kubernetes/terraform/k8_instances.tf
@@ -1,57 +1,59 @@
 // k8_instances
 
 // medium.com/@devopslearning/aws-iam-ec2-instance-role-using-terraform-fa2b21488536
-# Create IAM role in Terraform?
+# IAM role
 resource "aws_iam_role" "k8" {
   name = "k8_role"
   assume_role_policy = <<EOF
 {
-  "Version": "2012-10-17",
+  "Version": "2012-10-17", 
   "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
+    {"Effect": "Allow", "Principal": { "Service": "ec2.amazonaws.com"}, "Action": "sts:AssumeRole"}
   ]
 }
 EOF
   tags = {
-      tag-key = "iam_role_k8"
+       Name = "iam_role_k8"
   }
 }
 
-// Provide an IAM instance profile: https://www.terraform.io/docs/providers/aws/r/iam_instance_profile.html
+// IAM instance profile:
+//  www.terraform.io/docs/providers/aws/r/iam_instance_profile.html
 resource "aws_iam_instance_profile" "k8" {
   name = "k8_profile"
   role = "${aws_iam_role.k8.name}"
 }
 
-// Policy gives EC2 instance full access to S3 bucket
+// Role policy gives EC2 instance full access to S3 storage
 resource "aws_iam_role_policy" "k8" {
   name = "k8_policy"
   role = "${aws_iam_role.k8.id}"
-
   policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
-    {
-      "Action": [
-        "s3:*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
+    {"Action": ["s3:*"], "Effect": "Allow", "Resource": "*"}
   ]
 }
 EOF
 }
-// data "aws_iam_instance_profile" "k8" {
-//  name = "name_of_instance_profile" }
+
+resource "aws_iam_policy" "k8" {
+  name        = "k8_policy"
+  path        = "/"
+  description = "Kubernetes policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {"Effect": "Allow", "Action": ["ec2:*"], "Resource": ["*"]},
+    {"Effect": "Allow", "Action": ["elasticloadbalancing:*"], "Resource": ["*"]},
+    {"Effect": "Allow", "Action": ["route53:*"], "Resource": ["*"]},
+    {"Effect": "Allow", "Action": ["ecr:*"], "Resource": "*"}
+  ]
+}
+EOF
+}
 
 
 //-- KUBERNETES CONTROLLER  --
@@ -62,6 +64,7 @@ resource "aws_instance" "controller_0" {
   iam_instance_profile = "k8_profile"
   key_name = "${var.key_name}"
   private_ip = "10.240.0.10"
+  source_dest_check = false // enable traffic between foreign subnets
   subnet_id   = "${aws_subnet.k8.id}"
   vpc_security_group_ids = [aws_security_group.k8.id]
   tags = {
@@ -77,6 +80,7 @@ resource "aws_instance" "worker_0" {
   iam_instance_profile = "k8_profile"
   key_name = "${var.key_name}"
   private_ip = "10.240.0.20"
+  source_dest_check = false
   subnet_id   = "${aws_subnet.k8.id}"
   vpc_security_group_ids = [aws_security_group.k8.id]
   tags = {
